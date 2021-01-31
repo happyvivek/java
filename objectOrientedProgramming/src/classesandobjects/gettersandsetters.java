cb56d97976569d31d508f33189d863ec5b5ce9d3
vivesisi <vivesisi@VIVESISI-IN.oradev.oraclecorp.com>
2021-01-31 13:26:04 +0530

    Adding Access Modifiers

--- objectOrientedProgramming/src/classesandobjects/gettersandsetters.java
+++ objectOrientedProgramming/src/classesandobjects/gettersandsetters.java
@@ -5,8 +5,10 @@ package classesandobjects;
 //Like if somebody is entering roll number less than 0,we want to restrict that.
 //We have made the access to rollNumber as private so it cannot be accessed directly by object but indirectly using getter and setter functions.
 //You can access private variable within same class only and can be accessed by obj using functions like getters/setter
-
-
+//Access Modifiers Public/Private/Default
+//Default-Class is visible only within the same package,u need to import the class explicitly in 2nd package if u want to use class in pkg 1
+//Public -Class is visible to all packages anywhere
+//Private-Can be access only within the same class(Even obj within same package can access using getters and setters function.
 public class gettersandsetters {
 	
 	

b94b514a60fe38f1b52ea40ebfab0db058a72fa2
vivesisi <vivesisi@VIVESISI-IN.oradev.oraclecorp.com>
2021-01-31 13:20:41 +0530

    Explain getter/setters

--- objectOrientedProgramming/src/classesandobjects/gettersandsetters.java
+++ objectOrientedProgramming/src/classesandobjects/gettersandsetters.java
@@ -3,6 +3,8 @@ package classesandobjects;
 
 //The purpose of getter and setter is we want to bring the class within same package to follow certain conditions.
 //Like if somebody is entering roll number less than 0,we want to restrict that.
+//We have made the access to rollNumber as private so it cannot be acceses directly by object but indirectly using getter and setter functions.
+//You can access private variable withing same class only and can be accessed by obj using functions like getters/setter
 
 
 public class gettersandsetters {
